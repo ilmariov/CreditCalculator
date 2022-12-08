a19abbd19a74ce2e93d18ea5f9ee6ccd7f3c136b
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-12-07 22:18:18 -0500

    js working to calculate
    only using values from inputs but not interacting with options from selectors

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-const amount = document.getElementById("amount").value;
-const fee = document.getElementById("fee");
-console.log(amount);
-fee.style.color = "#ffcc00";
-fee.innerHTML = "<h3>Here goes the outcome: " + amount + "</h3>"; 
+const monthlyPay = (p, i, n) => {return p * (i * ((1 + i) ** n)) / (((1 + i) ** n) - 1)};
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+})
+
+const initApp = () => {
+    const calcForm = document.getElementById("calcForm");
+    calcForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const amount = document.getElementById("amount").value;
+        const months = document.getElementById("term").value;
+        const iRate = document.getElementById("rate").value / 100;
+        const toPay = monthlyPay(amount, iRate, months);
+        const div = document.getElementById("fee");
+        console.log(toPay);
+        div.innerHTML = "<h3>Monthly payment = " + Math.trunc(toPay) + "</h3>";
+    })
+}
+
